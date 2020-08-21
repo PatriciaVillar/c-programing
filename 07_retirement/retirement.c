11cb110f3a21dac90b85c79eea7f15d2a3d4a170
Coursera Learner <nobody@nowhere.nul>
2020-08-21 01:17:45 +0000

    Did assignment 07

--- 07_retirement/retirement.c
+++ 07_retirement/retirement.c
@@ -1,36 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct retire_info{
+typedef struct _retire_info{
   int months;
   double contribution;
-  double rate;
-} retire;
+  double rate_of_return;
+} retire_info;
 
 int main (){
-	   retire w1;
-	   w1.months=489;
-	   w1.contribution=1000;
-	   w1.rate=0.00375;
-	   retire r1;
-	   r1.months=384;
-	   r1.contribution=-4000;
-	   r1.rate=0.0008333333333333334;   
+	   retire_info working;
+	   working.months=489;
+	   working.contribution=1000;
+	   working.rate_of_return=0.00375;
+	   retire_info retired;
+	   retired.months=384;
+	   retired.contribution=-4000;
+	   retired.rate_of_return=0.0008333333333333334;   
 	   //retire_info working, //info about working
 	   //retire_info retired) //info about being retired
 int  startAge = 327;
 double initial = 21345;
 int Age = startAge;
 double savings = initial;
- for (int i=0;i < w1.months ; i++){
+ for (int i=0;i < working.months ; i++){
    //print Age  27 month  3 you have $21345.00
    printf("Age %3d month %2d you have $%.2f\n",Age/12,Age%12,savings);
-   savings=savings*(1+w1.rate)+w1.contribution;
+   savings=savings*(1+working.rate_of_return)+working.contribution;
 	   Age++;}
- for (int i=0;i < r1.months ; i++){
+ for (int i=0;i < retired.months ; i++){
    //print Age  27 month  3 you have $21345.00
    printf("Age %3d month %2d you have $%.2f\n",Age/12,Age%12,savings);
-   savings=savings*(1+r1.rate)+r1.contribution;
+   savings=savings*(1+retired.rate_of_return)+retired.contribution;
    Age++;}
  return EXIT_SUCCESS;
 }
